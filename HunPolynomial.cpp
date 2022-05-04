36d9d5fc9f3ce7e66d363755ff5b34a5635cc835
dcordova7146 <43242715+dcordova7146@users.noreply.github.com>
2022-05-04 17:08:54 -0400

    history class lol

---
+++ HunPolynomial.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "HunPolynomial.h"
+
+hpoly::hpoly(std::vector<int> poly){
+    ppoly = poly;
+}
+void hpoly::set(std::vector<int> poly){
+    ppoly = poly;
+}
+
+hpoly hpoly::operator+(hpoly& b){
+
+}
+hpoly hpoly::operator-(hpoly& b){
+
+}
+hpoly hpoly::operator*(hpoly& b){
+
+}
+hpoly hpoly::operator<<(hpoly& b){
+
+}
---
+++ HunPolynomial.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+
+class hpoly{
+
+    private:
+    std::vector<int> ppoly;
+
+    public:
+    hpoly(); //creates an empty polynomial
+    hpoly(std::vector<int> poly); // set ppoly to poly
+    void set(std::vector<int> poly); // same as parameterized constructor
+    hpoly operator+ (hpoly& b);
+    hpoly operator- (hpoly& b);
+    hpoly operator* (hpoly& b);
+    hpoly operator<< (hpoly& b);
+    //ostream& operator<<(ostream& os, const hpoly& dt) not a class member
+    
+};
+
