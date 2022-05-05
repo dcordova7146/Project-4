b45ccbe23a532f356bcac0a96e34f2fa72e1dd7a
dcordova7146 <43242715+dcordova7146@users.noreply.github.com>
2022-05-05 13:26:03 -0400

    udate

--- HunPolynomial.cpp
+++ HunPolynomial.cpp
@@ -22,126 +22,82 @@ public:
     friend HunPolynomial operator*(const HunPolynomial &p1, const HunPolynomial &p2);
 };
 
-HunPolynomial::HunPolynomial(/* args */) {}
+HunPolynomial::HunPolynomial(/* args */)
+{
+    polynomial = {0};
+}
 
 HunPolynomial::HunPolynomial(std::vector<int> input)
 {
-    while (input.size() > 0)
-    {
-        polynomial.push_back(input.back());
-        input.pop_back();
-    }
+    polynomial = input;
+    std::reverse(polynomial.begin(), polynomial.end());
 };
 
 HunPolynomial::~HunPolynomial(){};
 
 void HunPolynomial::Set(std::vector<int> input)
 {
-    while (input.size() > 0)
-    {
-        polynomial.push_back(input.back());
-        input.pop_back();
-    }
+    polynomial = input;
+    std::reverse(polynomial.begin(), polynomial.end());
 }
 
 std::ostream &operator<<(std::ostream &out, const HunPolynomial &hp)
 {
     std::string buffer = "";
-    bool addterm = true;
     int i;
     // std::cout << buffer.length() << std::endl;
 
     for (i = hp.polynomial.size() - 1; i >= 0; i--) // for each term in vector
     {
-
-        // buffer.append(std::to_string(hp.polynomial[i]) + "x" + "^" + std::to_string(i) + " ");
-
-        if (buffer.length() == 0)// base case
+        if (hp.polynomial[i] != 0) // add term if coefficient is not zero
         {
-            // check pos or neg 
-            if ((hp.polynomial[i] > 1) || (hp.polynomial[i] < 1 && hp.polynomial[i] > 0)) // pos coefficient reguar
+            if (buffer.length() != 0) // base case
+            /*check if not base case to add sign, otherwise are all these operations the same?*/
             {
-                buffer.append(std::to_string(hp.polynomial[i]));
-                buffer.append("*");
-            }
-            else if (hp.polynomial[i] == 1) // do not place coefficient
-            {
-                // skip adding coefficient
-            }
-            else if (hp.polynomial[i] == 0) // do not place a term
-            {
-                addterm = false;
-            }
-            else if (hp.polynomial[i] == -1)
-            {
-                buffer.append("- ");
-            }
-            else if ((hp.polynomial[i] < 0) || (hp.polynomial[i] < 0 && hp.polynomial[i] > -1)) // negative coefficient
-            {
-                buffer.append("- ");
-                buffer.append(std::to_string(std::abs(hp.polynomial[i])));
-                buffer.append("*");
-            }
-            else // invalid term?
-            {
-                std::cout << "error invalid term in polynomial?" << std::endl;
-                exit(0);
-            }
-        }
-        else if ((hp.polynomial[i] > 1) || (hp.polynomial[i] < 1 && hp.polynomial[i] > 0))
-        {
-            buffer.append("+");
-            buffer.append(" ");
-            buffer.append(std::to_string(hp.polynomial[i]));
-            buffer.append("*");
-        }
-        else if (hp.polynomial[i] == 1)
-        {
-            buffer.append("+");
-            buffer.append(" ");
-        }
-        else if (hp.polynomial[i] == 0)
-        {
-            addterm = false;
-        }
-        else if (hp.polynomial[i] == -1)
-        {
-            buffer.append("-");
-            buffer.append(" ");
-        }
-        else if ((hp.polynomial[i] < 0) || (hp.polynomial[i] < 0 && hp.polynomial[i] > -1))
-        {
-            buffer.append("-");
-            buffer.append(" ");
-            buffer.append(std::to_string(std::abs(hp.polynomial[i])));
-        }
-
-        if (addterm == true) // true if coefficient is not 0
-        {
-            switch (i)
-            {
-            case 1:
-                buffer.append("x");
                 buffer.append(" ");
-                break;
-            case 0:
-                if (hp.polynomial[i] == 1)
+                if (hp.polynomial[i] < 0)
+                {
+                    buffer.append("-");
+                }
+                else if (hp.polynomial[i] > 0)
+                {
+                    buffer.append("+");
+                }
+                buffer.append(" ");
+            }
+            else if (buffer.length() == 0 && hp.polynomial[i] < 0)
+            {
+                buffer.append("-");
+                buffer.append(" ");
+            }
+
+            if ((hp.polynomial[i] == 1) || (hp.polynomial[i] == -1))
+            {
+                if (i == 0)
                 {
                     buffer.append("1");
                 }
-                else if (hp.polynomial[i] > 1)
-                { // this is janky
-                    buffer.pop_back();
+            }
+            else
+            {
+                buffer.append(std::to_string(std::abs(hp.polynomial[i])));
+                if (i != 0)
+                {
+                    buffer.append("*");
                 }
-                break;
-            default:
+            }
+
+            if (i == 1)
+            {
+                buffer.append("x");
+            }
+            else if (i != 0)
+            {
                 buffer.append("x");
                 buffer.append("^");
                 buffer.append(std::to_string(i));
-                buffer.append(" ");
             }
         }
-        addterm = true;
 
         // std::cout << "index: " << i << " num:  " << hp.polynomial[i] << " pow:  " << i <<std::endl;
     }
@@ -154,31 +110,54 @@ std::ostream &operator<<(std::ostream &out, const HunPolynomial &hp)
 
 HunPolynomial operator+(const HunPolynomial &p1, const HunPolynomial &p2)
 {
-    std::vector<int> buffer = p1.polynomial.size() > p2.polynomial.size() ? p1.polynomial : p2.polynomial;
-    std::vector<int> smallerVector = p1.polynomial.size() < p2.polynomial.size() ? p1.polynomial : p2.polynomial;
-    for (int i = smallerVector.size(); i >= 0; i--)
-    {
-        std::cout << "index: " << i << " num:  " << buffer[i] << " pow:  " << i << std::endl;
-        buffer[i] += smallerVector[i];
+    std::vector<int> bigBuffer = p1.polynomial.size() >= p2.polynomial.size() ? p1.polynomial : p2.polynomial;
+    std::vector<int> smallBuffer = p1.polynomial.size() < p2.polynomial.size() ? p1.polynomial : p2.polynomial;
+
+    while(bigBuffer.size() != smallBuffer.size()){
+        smallBuffer.push_back(0);
     }
-    std::reverse(buffer.begin(), buffer.end());
-    return buffer;
+
+    std::transform (bigBuffer.begin(), bigBuffer.end(), smallBuffer.begin(), bigBuffer.begin(), std::plus<int>());
+
+    std::reverse(bigBuffer.begin(), bigBuffer.end());
+    return bigBuffer;
+
+
+}
+
+HunPolynomial operator-(const HunPolynomial &p1, const HunPolynomial &p2)
+{
+    std::vector<int> bigBuffer = p1.polynomial.size() >= p2.polynomial.size() ? p1.polynomial : p2.polynomial;
+    std::vector<int> smallBuffer = p1.polynomial.size() < p2.polynomial.size() ? p1.polynomial : p2.polynomial;
+
+    while(bigBuffer.size() != smallBuffer.size()){
+        smallBuffer.push_back(0);
+    }
+
+    for (int i = 0; i < smallBuffer.size(); i++){
+        smallBuffer[i] *= -1;
+    }
+
+    std::transform (bigBuffer.begin(), bigBuffer.end(), smallBuffer.begin(), bigBuffer.begin(), std::plus<int>());
+
+    std::reverse(bigBuffer.begin(), bigBuffer.end());
+    return bigBuffer;
 }
 
 int main()
 {
     HunPolynomial defPol;
 
-    // Printing tests. Make sure your output matches the expected output EXACTLY
-    defPol.Set({1, 15, -1, 20});
-    std::cout << defPol << std::endl;
-    std::cout << "x^3 + 15*x^2 - x + 20  <= that is what you should see above exactly\n\n";
+    // // Printing tests. Make sure your output matches the expected output EXACTLY
+    // defPol.Set({1, 15, -1, 20});
+    // std::cout << defPol << std::endl;
+    // std::cout << "x^3 + 15*x^2 - x + 20  <= that is what you should see above exactly\n\n";
 
-    HunPolynomial defPol2;
+    // HunPolynomial defPol2;
 
-    defPol2.Set({-1, 0, 1});
-    std::cout << defPol2 << std::endl;
-    std::cout << "- x^2 + 1  <= that is what you should see above exactly\n\n";
+    // defPol2.Set({-1, 0, 1});
+    // std::cout << defPol2 << std::endl;
+    // std::cout << "- x^2 + 1  <= that is what you should see above exactly\n\n";
 
     // Testing operator()
     // std::cout << "The value is " << defPol(-2.5) << ". The correct value is -5.25\n\n";
@@ -189,9 +168,43 @@ int main()
     HunPolynomial Result;
     HunPolynomial Empty; // that would be an empty polinomial
 
-    Result = X + Y;
+    // Result = X + Y;
+    // std::cout << Result << std::endl;
+    // std::cout << "- 4*x^4 + 5*x^3 + 4*x^2 - x - 31  <= that is what you should see above exactly\n\n";
+
+    // Result = Y + X;
+    // std::cout << Result << std::endl;
+    // std::cout << "- 4*x^4 + 5*x^3 + 4*x^2 - x - 31  <= that is what you should see above exactly\n\n"; // testing for deconstruction? or clear cache?
+
+    // Result = X + Empty; // segmentation default core dump
+    // std::cout << Result << std::endl;
+    // std::cout << "- 4*x^4 + x^2 - 31  <= that is what you should see above exactly\n\n";
+
+    // Result = Empty + Y;
+    // std::cout << Result << std::endl;
+    // std::cout << "5*x^3 + 3*x^2 - x  <= that is what you should see above exactly\n\n";
+
+    // HunPolynomial Z{{-5, -4, 2, 0}};
+    // Result = Y + Z;
+    // std::cout << Result << std::endl;
+    // std::cout << "- x^2 + x  <= that is what you should see above exactly\n\n";
+
+    // Testing subtraction
+    Result = X - Y;
     std::cout << Result << std::endl;
-    std::cout << "- 4*x^4 + 5*x^3 + 4*x^2 - x - 31  <= that is what you should see above exactly\n\n";
+    std::cout << "- 4*x^4 - 5*x^3 - 2*x^2 + x - 31  <= that is what you should see above exactly\n\n";
+
+    Result = Y - X;
+    std::cout << Result << std::endl;
+    std::cout << "4*x^4 + 5*x^3 + 2*x^2 - x + 31  <= that is what you should see above exactly\n\n";
+
+    Result = Y - Empty;
+    std::cout << Result << std::endl;
+    std::cout << "5*x^3 + 3*x^2 - x  <= that is what you should see above exactly\n\n";
+
+    Result = Empty - Y;
+    std::cout << Result << std::endl;
+    std::cout << "- 5*x^3 - 3*x^2 + x  <= that is what you should see above exactly\n\n";
 
     return 0;
 }
